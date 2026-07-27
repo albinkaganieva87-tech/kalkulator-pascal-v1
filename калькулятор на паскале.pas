4d1df9b7d4c28b2199bf4ca8a3f7512c232d93b1
Abobix inc. <albinka.ganieva.87@mail.ru>
2026-07-27 15:24:40 +0300

    Add files via upload

---
+++ калькулятор на паскале.pas
@@ -0,0 +1,16 @@
+﻿program z4;
+ var a,b:integer;
+  rez:real;
+  p:char;
+begin
+ readln(a);
+ readln(p);
+ readln(b);
+ case p of
+  '+': rez:=a+b;
+  '-': rez:=a-b;
+  '*': rez:=a*b;
+  '/': rez:=a/b;
+end;
+writeln(rez);
+end.
